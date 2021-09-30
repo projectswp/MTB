e6589b82d866a5dbd276930574931e64d05c1199
baohieuuuuu <hieuntbde150107@fpt.edu.vn>
2021-09-30 21:41:48 +0700

    change

--- js/modalHome.js
+++ js/modalHome.js
@@ -1,9 +1,6 @@
 document.getElementById('button').addEventListener('click',function(){
     document.querySelector('.bg-modal').style.display='flex';
 });
-document.getElementById('button1').addEventListener('click',function(){
-    document.querySelector('.bg-modal').style.display='flex';
-});
 document.querySelector('.close').addEventListener('click',function(){
     document.querySelector('.bg-modal').style.display='none';
 });
