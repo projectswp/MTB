821c52e77596af68f420a70226f3d62fbc3eef1d
baohieuuuuu <hieuntbde150107@fpt.edu.vn>
2021-10-11 01:04:30 +0700

    change

--- js/search.js
+++ js/search.js
@@ -1,14 +1,14 @@
-document.getElementById('searchIcon').onclick = function() {
-    document.getElementById('search').style.display = 'block';
-    document.getElementById('clear').style.display = 'block';
-    document.getElementById('search').focus();
-    document.getElementById('searchIcon').style.display = 'none';
-  }
-  document.getElementById('clear').onclick = function() {
-    document.getElementById('searchIcon').style.display = 'block';
-    document.getElementById('search').style.display = 'none';
-    document.getElementById('clear').style.display = 'none';
-  }
+// document.getElementById('searchIcon').onclick = function() {
+//     document.getElementById('search').style.display = 'block';
+//     document.getElementById('clear').style.display = 'block';
+//     document.getElementById('search').focus();
+//     document.getElementById('searchIcon').style.display = 'none';
+//   }
+//   document.getElementById('clear').onclick = function() {
+//     document.getElementById('searchIcon').style.display = 'block';
+//     document.getElementById('search').style.display = 'none';
+//     document.getElementById('clear').style.display = 'none';
+//   }
 
 // const input = document.getElementById("searchInput");
 // const searchBtn = document.getElementById("searchBtn");
@@ -19,3 +19,8 @@ document.getElementById('searchIcon').onclick = function() {
 // };
 
 // searchBtn.addEventListener("click", expand); 
+$('.search-button').on('click', function() {
+  $('.toggle-target-container').animate({width:'toggle'},500); //350 ở đây là tốc đô (ms)
+  $('.search-group').toggleClass('close-search open-search');
+  $("#search-icon").toggleClass('fa-search fa-times');
+});
